0b7bbbdcd25f25a2fbf3395db7aea7ff120f79f2
张君勖 <zjunxu1989@gmail.com>
2026-02-15 20:15:37 +0800

    chore: bootstrap ios app

---
+++ AICamera/AICamera/AICameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AICameraApp.swift
+//  AICamera
+//
+//  Created by zhangjunxu3 on 2026/2/15.
+//
+
+import SwiftUI
+
+@main
+struct AICameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AICamera/AICamera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AICamera
+//
+//  Created by zhangjunxu3 on 2026/2/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
